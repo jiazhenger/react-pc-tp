13664b8c8e16e365cfb728e885669943ce4f84bc
jiazer <464893998@qq.com>
2020-07-28 18:13:47 +0800

    0

--- src/common/public/utils.js
+++ src/common/public/utils.js
@@ -79,6 +79,14 @@ const _ = {
 	setTitle(text){ window.document.title = text },
 	// 深拷贝
 	copy(data){ return JSON.parse(JSON.stringify(data)) },
+	// 添加 className
+	css(className, defaultValue){ 
+		if(arguments.length === 1) {
+			return className ? className : ''
+		}else if(arguments.length === 2){
+			return className ? className : 'defaultValue'
+		}
+	},
 	// ========================================================================  设置 model
 	async setModel(_this,keyValue,option){
 		const opt = {
--- src/components-x/content.js
+++ src/components-x/content.js
@@ -1,4 +1,4 @@
-﻿/* ====================================== 滚动条  ====================================== */
+/* ====================================== 滚动条  ====================================== */
 import React from 'react'
 // =====================================================================
 /*
@@ -17,46 +17,23 @@ const Fv = ({ className, header, footer, contentClassName, children }) => (
 	</Content>
 )
 */
-const Page = props => <Content isPage {...props} />
 export default class Content extends React.Component{
 //	static Fv = Fv
-	static Page = Page
 	render(){
-		const { id, className, style, children, onClick, scrollY, scrollX, scrollXY, isPage, pageClass, wraperStyle, title, subTitle, headerSuffix, isFull } = this.props
+		const { id, className, style, children, onClick, scrollY, scrollX, scrollXY } = this.props
 		let scroll = 'oys'
 		if(scrollX){ scroll = 'oxs' }
 		if(scrollY){ scroll = 'oys' }
-		if(scrollXY || isPage){ scroll = 'oxys' }
-		if(scrollY === false){ scroll = null }
+		if(scrollXY){ scroll = 'oxys' }
+		scroll = scrollY === false ? '' : scroll + ' scrollbar'
 		return (
 			<div 
 				id 			={ id } 
-				className	={ `abs_lt wh ${scroll||''} ${className||'bcb'}` }
+				className	={ `abs_lt wh ${scroll} ${ className || 'bcb' }` }
 				style		={ style } 
 				onClick		={ onClick }
 			>
-				{
-					isPage ? (
-						<section 
-							style={{padding:`${title?'0':'15px'} 15px 15px`,minWidth:'1000px',minHeight:'800px',...wraperStyle}} 
-							className={`${pageClass||''} ${isFull?'h':''}`}
-						>
-							{
-								title && (
-									<header className='h50 fxm'>
-										<div className='ex fx'>
-											<h2 className='b f18'>{title}</h2>
-											<div className='f16'>{subTitle}</div>
-										</div>
-										{ headerSuffix }
-									</header>
-								)
-							}
-							
-							{children}
-						</section>
-					) : <>{children}</>
-				}
+				{children}
 			</div>
 		)
 	}
