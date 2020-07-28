1a464060effd2a3b0d8e9f3427a24a853fe5ceae
jiazer <464893998@qq.com>
2020-07-28 17:14:08 +0800

    0

--- src/components-x/data-loading.js
+++ src/components-x/data-loading.js
@@ -1,9 +1,8 @@
-﻿/* ====================================== 页面加载效果  ====================================== */
+/* ====================================== 页面加载效果  ====================================== */
 import React from 'react'
 import AddLast from './add-last'
 const $fn = window.$fn
 // ===================================================================== data-loading
-
 const Loading = ({ color, size }) => (
     <svg width={size} height={size} viewBox='0 0 100 100'>
 		<circle fill='none' stroke='#fff' strokeWidth='4' cx='50' cy='50' r='44' style={{opacity:0.6}}></circle>
@@ -15,11 +14,11 @@ const Loading = ({ color, size }) => (
 
 export default () => (
 	<AddLast name='data-loading'>
-		<div id='ubLoading' className='fix_lt wh tc dn' style={{zIndex:1999}}>
+		<div id='jzer-loading' className='fix_lt wh tc dn' style={{zIndex:1999}}>
 			<div className='fxmc wh'>
 				<div className='r8px' style={{background:'rgba(0,0,0,.8)',minWidth:'90px',padding:'8px 12px'}}>
 					<div className='fxmc'><Loading size='60px' /></div>
-					<div className='cf f12 lh20 mt5 ub-loading-msg'></div>
+					<div className='cf f12 lh20 mt5 jzer-loading-msg'></div>
 				</div>
 			</div>
 		</div>
--- src/components-x/toast.js
+++ src/components-x/toast.js
@@ -1,11 +1,11 @@
-﻿import React from 'react'
+import React from 'react'
 import AddLast from './add-last'
 // ===================================================================== toast
 export default () => (
 	<AddLast name='toast'>
-		<div id='ubToast' className='ub-toast fix_lt wh tc dn' style={{zIndex:2000}}>
+		<div id='jzer-toast' className='fix_lt wh tc dn' style={{zIndex:2000}}>
 			<div className='fxmc wh xplr'>
-				<p className='ub-toast r5px cf p10 f13' style={{background:'rgba(0,0,0,0.6)'}}></p>
+				<p className='jzer-toast-msg r5px cf p10 f13' style={{background:'rgba(0,0,0,0.6)'}}></p>
 			</div>
 		</div>
 	</AddLast>
