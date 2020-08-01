c72fa398fef641dc8847b1459997e7fa730c5aa2
jiazer <464893998@qq.com>
2020-08-01 13:37:42 +0800

    0

--- src/common/utils/data-type.js
+++ src/common/utils/data-type.js
@@ -3,7 +3,7 @@ const _ = {
 	hasArray 	: d => _.isArray(d) && d.length > 0,
 	hasObject 	: d => _.isObject(d) && Object.keys(d).length > 0,
 	isEmpty 	: d => _ === null || d === undefined || d === '',
-	isValid 	: d => !this.isEmpty(d) || d === 0 || d === false
+	isValid 	: d => !_.isEmpty(d) || d === 0 || d === false
 };
 
 (['String', 'Number', 'Array', 'Object', 'Boolean', 'Function', 'Undefined']).forEach(v =>  _['is' + v] = obj => ( {}.toString.call(obj) === '[object '+ v +']' ) )
