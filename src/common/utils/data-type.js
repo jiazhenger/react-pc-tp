408f5d95d0b53788d85d703d66241768bdba9e00
jiazer <464893998@qq.com>
2020-11-30 09:33:51 +0800

    Update data-type.js

--- src/common/utils/data-type.js
+++ src/common/utils/data-type.js
@@ -1,11 +1,11 @@
 /* ====================================== 数据类型  ====================================== */
-const _ = {
-	hasArray 	: d => _.isArray(d) && d.length > 0,
-	hasObject 	: d => _.isObject(d) && Object.keys(d).length > 0,
-	isEmpty 	: d => _ === null || d === undefined || d === '',
-	isValid 	: d => !_.isEmpty(d) || d === 0 || d === false
+const $ = {
+	hasArray 	: d => $.isArray(d) && d.length > 0,
+	hasObject 	: d => $.isObject(d) && Object.keys(d).length > 0,
+	isEmpty 	: d => d === null || d === undefined || d === '',
+	isValid 	: d => !$.isEmpty(d) || d === 0 || d === false
 };
 
-(['String', 'Number', 'Array', 'Object', 'Boolean', 'Function', 'Undefined']).forEach(v =>  _['is' + v] = obj => ( {}.toString.call(obj) === '[object '+ v +']' ) )
+(['String', 'Number', 'Array', 'Object', 'Boolean', 'Function', 'Undefined']).forEach(v =>  $['is' + v] = obj => ( {}.toString.call(obj) === '[object '+ v +']' ) )
 
-export default _
+export default $
