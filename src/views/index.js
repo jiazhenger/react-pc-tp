8e4f4ed273a894b2acfc0642311f9208fbe916f0
jiazer <464893998@qq.com>
2021-01-18 18:24:19 +0800

    0

--- src/views/index.js
+++ src/views/index.js
@@ -8,21 +8,11 @@ const Page = window.$async(()=>import('#tp/content/content-page'))
 
 // ===================================================================== declare
 
-const F = ({ Child }) => {
-	return (<div><Child data='123' /></div>)
-};
-
-class M extends React.Component{
-	render(){
-		return (<div><this.props.Child data='贾正双' /></div>)
-	}
-};
 // ===================================================================== component
 const Index = (props) => {
 	return (
 		<Page>
 			<Link to='/login'>首页</Link>
-			<M Child={props => <div>{ props.data } </div>} />
 		</Page>
 	)
 }
